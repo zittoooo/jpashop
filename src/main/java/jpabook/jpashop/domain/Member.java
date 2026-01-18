811ae5df5e6cdc335497a22950557be16ab6ab37
jiholee <stellar7484@naver.com>
2026-01-18 22:40:40 +0900

    도메인 분석 설계 - 테이블 생성

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")  // order 테이블에 있는 member필드에 맵핑되었다, 읽기 전용
+    private List<Order> orders = new ArrayList<>();
+}
---
+++ src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Album extends Item {
+    private String artist;
+    private String ect;
+}
